70f6cf5bed721f1033ed448a31ca553595ed3935
Stacy Holtz <stacyholtz6@gmail.com>
2020-02-08 10:01:45 -0700

    init tabs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,7 @@
+// Tabs
+const tabs = document.querySelectorAll('.tabs');
+M.Tabs.init(tabs, {});
+
 // Parallax
 const parallax = document.querySelectorAll('.parallax');
 M.Parallax.init(parallax, {});
-
