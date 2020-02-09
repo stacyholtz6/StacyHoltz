2223cbd5ed7b2e705590a6b314b768b48d3a2f09
Stacy Holtz <stacyholtz6@gmail.com>
2020-02-09 11:36:12 -0700

    added tabs back

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,15 @@
+// Sidenav
+const sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav);
+
 // Parallax
 const parallax = document.querySelectorAll('.parallax');
 M.Parallax.init(parallax);
+
+// tabs
+const tabs = document.querySelectorAll('.tabs');
+M.Tabs.init(tabs);
+
+// scrollspy
+const scrollspy = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(scrollspy);
