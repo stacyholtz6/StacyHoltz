885a73a6324cc3205032c6e21f2ee9fb0593c25f
Stacy Holtz <stacyholtz6@gmail.com>
2020-02-09 10:40:16 -0700

    fixed parallax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,3 @@
-// Tabs
-const tabs = document.querySelectorAll('.tabs');
-M.Tabs.init(tabs, {});
-
 // Parallax
 const parallax = document.querySelectorAll('.parallax');
-M.Parallax.init(parallax, {});
+M.Parallax.init(parallax);
